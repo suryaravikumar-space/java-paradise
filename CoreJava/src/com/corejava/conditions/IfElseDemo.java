ba717b75de7d2fc54cae7480d510c8d12a9454fa
suryaravikumar-space <suryaravikumar1729@gmail.com>
2022-09-15 07:05:24 +0530

    if condition added

--- CoreJava/src/com/corejava/conditions/IfElseDemo.java
+++ CoreJava/src/com/corejava/conditions/IfElseDemo.java
@@ -1,7 +1,16 @@
 package com.corejava.conditions;
 
 public class IfElseDemo {
+    static void voterAge(){
+        int age = 18;
+        int userInput = 17;
+        if(age<=userInput){
+            System.out.println("allow");
+        }else {
+            System.out.println("not allow");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("hello java");
+        voterAge();
     }
 }
